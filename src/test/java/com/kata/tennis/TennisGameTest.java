b2a5c2547c90500803e72ec3d1a736cd6c4d9969
Atul Mittal <55511902+atulmittalg@users.noreply.github.com>
2020-03-08 17:12:10 +0100

    Refactor: using local TennisGame object for the test cases

--- src/test/java/com/kata/tennis/TennisGameTest.java
+++ src/test/java/com/kata/tennis/TennisGameTest.java
@@ -3,21 +3,27 @@ package com.kata.tennis;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TennisGameTest
 {
+    private TennisGame tennisGame;
+
+    @Before
+    public void initializeBowlingObj(){
+        tennisGame = new TennisGame();
+    }
+
     @Test
     public void shouldReturnLoveAllAtStartOfNewGame()
     {
-        TennisGame tennisGame = new TennisGame();
         Assert.assertEquals("Love All", tennisGame.getResult());
     }
 
     @Test
     public void shouldReturnFifteenLoveWhenPlayerOneScores()
     {
-        TennisGame tennisGame = new TennisGame();
         tennisGame.playerOneScores();
         Assert.assertEquals("Fifteen, Love", tennisGame.getResult());
     }
