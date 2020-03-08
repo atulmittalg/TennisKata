a84fc296eccf1c69c0b981b0b93bab1ddca2ed62
Atul Mittal <55511902+atulmittalg@users.noreply.github.com>
2020-03-08 23:52:33 +0100

    Refactor: Removing unwanted constants

--- src/main/java/com/kata/tennis/constants/ScoreConstants.java
+++ src/main/java/com/kata/tennis/constants/ScoreConstants.java
@@ -3,14 +3,11 @@ package com.kata.tennis.constants;
 import java.util.HashMap;
 
 public class ScoreConstants {
-    public static final int LOVE = 0;
-    public static final int FIFTEEN = 1;
-    public static final int THIRTY = 2;
     public static final int FORTY = 3;
     public static final HashMap<Integer, String> scoreValues = new HashMap<Integer, String>(){{
-        put(LOVE, "Love");
-        put(FIFTEEN, "Fifteen");
-        put(THIRTY, "Thirty");
+        put(0, "Love");
+        put(1, "Fifteen");
+        put(2, "Thirty");
         put(FORTY, "Forty");
     }};
 }
